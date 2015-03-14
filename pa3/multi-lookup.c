d282875d0e81f42a5089dc0a359fa3e2db9c9257
erik-feller <erik.feller@colorado.edu>
2015-03-14 16:21:15 +0000

    Added some skeletons of functions, not in header

--- pa3/multi-lookup.c
+++ pa3/multi-lookup.c
@@ -1,1 +1,30 @@
 //The main file
+
+//Includes
+#include "queue.h" //This is given by the OS prof
+#include "util.h" //Also given by the OS prof
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <strings.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#define QMAXSIZE 10
+
+//Should have returns for the different errors that are given in the assignment.
+int load(file){ //Function to handle building a queue from the file.
+		
+}
+
+//These functions should have different returns for the errors that were outlined in the assignment file. 
+int reswrite(queue){ //Function to resolve hostnames and then write to the file.)
+
+}
+void main(int argc, char *argv){
+		//Iterate over the *argv array giving the names of files and performing operations on each one. 
+		int i = 2;
+			
+
+			
+}
