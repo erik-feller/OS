d33c9bc7a996b7496b8acec0a69a5fda7ef7d5c7
erik-feller <erik.feller@colorado.edu>
2015-04-04 17:22:21 +0000

    Initial commit

---
+++ pa4/testharness.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+#*********************************************************************
+#Script to run and time the various outputs from the scheduling commands. 
+#May or may not be responsible for forking into different processes
+#Will automatically run all of the tests 5 times and average them for consistency
+#Planning on making it execute all 27 tests at onece
+#If I feel fancy it'll output a .csv file to use in excel. But I probably won't
+#*********************************************************************
+
+
